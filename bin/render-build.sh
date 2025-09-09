98331fa374528e6471df45ed41b1612e7e5b1cfc
fbarreto05 <fernandobarreto864@gmail.com>
2025-09-09 09:52:38 -0300

    give permissions

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash# exit on error
-
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+./bin/rails assets:precompile
+./bin/rails assets:clean
